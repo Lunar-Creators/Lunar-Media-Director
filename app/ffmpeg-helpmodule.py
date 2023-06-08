09ddcd27113d55284fbb57752012de29c1ae5590
SHULKER <92025362+SHULKERPLAY@users.noreply.github.com>
2023-06-08 12:07:35 +0000

    - Fixed exit with code error - Fixed wrong indexing of values in selectors - Fixed subproccess check - Optimized functions of loading and leaving modules - Added some coments

--- app/ffmpeg-helpmodule.py
+++ app/ffmpeg-helpmodule.py
@@ -1,34 +1,51 @@
-print('\033[3m\033[33m{}\033[0m'.format('Loading...'))
 import os
 import subprocess
-os.system('cls' if os.name == 'nt' else 'clear')
+import runpy
+import time
+
+def loading():
+    print('\033[3m\033[33m{}\033[0m'.format('Loading...'))
+
+loading()
+
+def wronginput():
+    print('\033[1m\033[91m{}\033[0m'.format('INCORRECT INPUT'))
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def leavemodule():
     print('\033[3m\033[33m{}\033[0m'.format('Leaving...'))
+
+
+# Universal part
 
 def helpmenu():
     print('\033[1m{}\033[0m'.format('\nHow can ffmpeg help you?'),'\n--------------------------\n  1 - Standart list of help\n  2 - Very long List of help\n  3 - EXTREAMLY LONG LIST OF HELP\n  4 - Flag help for available ffmpeg encoders\n  5 - List of available ffmpeg encoders\n  Q - Quit Help\n--------------------------\n','\033[35m{}\033[0m'.format('NOTE! In commandline mode, you can enter the help command with your arguments'))
     choice = input("Type your choice: ")
     if choice == '1':
-        subprocess.run('ffmpeg -h')
+        subprocess.run('ffmpeg -h', check=False)
         helpmenu()
     if choice == '2':
-        subprocess.run('ffmpeg -h long')
+        subprocess.run('ffmpeg -h long', check=False)
         helpmenu()
     if choice == '3':
-        subprocess.run('ffmpeg -h full')
+        subprocess.run('ffmpeg -h full', check=False)
         helpmenu()
     if choice == '4':
-        subprocess.run('ffmpeg -encoders')
+        subprocess.run('ffmpeg -encoders', check=False)
         print('\033[1m{}\033[0m'.format('\n\nEnter the one you are interested in from the list'),'\033[33m{}\033[0m'.format('\n(example: libvpx-vp9)'),)
         encoder = input("You need help with: ")
-        subprocess.run(f'ffmpeg -h encoder={encoder}')
+        subprocess.run(f'ffmpeg -h encoder={encoder}', check=False)
         del encoder
         helpmenu()
     if choice == '5':
-        subprocess.run('ffmpeg -encoders')
+        subprocess.run('ffmpeg -encoders', check=False)
         helpmenu()
     if choice == 'Q':
         leavemodule()
+    else:
+        wronginput()
+        helpmenu()
+os.system('cls' if os.name == 'nt' else 'clear')
 helpmenu()    
 
--- app/lunardirector-mainmenu.py
+++ app/lunardirector-mainmenu.py
@@ -1,36 +1,20 @@
-print('\033[3m\033[33m{}\033[0m'.format('Loading...'))
-
 import os
 import subprocess
 import runpy
 import time
 
-def reset():
-    del filepath 
-    del inputaudio 
-    del encoder 
-    del outputformat 
-    del outputname 
-    del predel 
-    del Profile 
-    del tune 
-    del vidbitrate 
-    del audiocodec 
-    del threads 
-    del audiobitrate 
-    del flags 
-    del subencoder 
-    del outputfolder 
-    del inputsubtitle 
-    del disablevideo 
-    del disableaudio 
-    del framerate 
-    del size 
-    del volume 
-    del disablesubtitles 
-    del audiotype 
-    del maxbitrate 
-    del choice
+def loading():
+    print('\033[3m\033[33m{}\033[0m'.format('Loading...'))
+# LOADING AFTER DEFINING FUNCTION
+loading()
+
+def wronginput():
+    print('\033[1m\033[91m{}\033[0m'.format('INCORRECT INPUT'))
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def leavemodule():
+    print('\033[3m\033[33m{}\033[0m'.format('Leaving...'))
 
 # exec = 'ffmpeg'
 # optlist = f' {exec} input'
@@ -39,27 +23,29 @@ def reset():
 # opts = '-i F:\intro720.mov -c:v copy F:\intpython.mov'
 # subprocess.run(f'ffmpeg {opts}')
 
+
+
 def welcome():
     os.system('cls' if os.name == 'nt' else 'clear')
     print('\033[3m\033[33m{}\033[0m'.format('--appver unknown -indev'),'\033[3m\033[91m{}\033[0m'.format('--copyright "Lunar Creators"\n'))
     print('If you have a problem, or you want to suggest a preset or feature to add, please contact us!\n','\033[1m{}\033[0m'.format('WELCOME TO LUNAR MEDIA DIRECTOR! (CLI Version)'),'\n \n-------------------------- \n  H - FFmpeg Help\n  Y - Select Video Preset\n  A - Audio Converting\n  P - Photo Converting\n  T - Select Tool\n  Q - Half-Manual Mode\n  K - Audio to Video Encoding\n  C - Commandline mode\n  X - Our Github\n  V - Open Video Downloader\n  E - Exit \n--------------------------')
     choice = input("Type your choice: ")
-    if choice == 'H':
+    if choice == 'H' or choice == 'h':
         runpy.run_module(mod_name='ffmpeg-helpmodule')
         welcome()
-    if choice == 'Y':
+    if choice == 'Y' or choice == 'y':
         void()
-    if choice == 'A':
+    if choice == 'A' or choice == 'a':
         void()
-    if choice == 'P':
+    if choice == 'P' or choice == 'p':
         void()
-    if choice == 'T':
+    if choice == 'T' or choice == 't':
         void()
-    if choice == 'Q':
+    if choice == 'Q' or choice == 'q':
         void()
-    if choice == 'K':
+    if choice == 'K' or choice == 'k':
         void()
-    if choice == 'C':
+    if choice == 'C' or choice == 'c':
         os.system('cls' if os.name == 'nt' else 'clear')
         print('In this mode you can run your command with your flags for ffmpeg\nUse this mode if you fully know what you are doing. "-h" for help.\nType "quit" to exit\n\nEnter ffmpeg arguments directly,\nexample: "','\033[32m{}\033[0m'.format('-i input -c:v libvpx -crf 16 -an -lag-in-frames 16 -auto-alt-ref 0 -y output'),'"')
         def supercustom():
@@ -67,21 +53,19 @@ def welcome():
             if cmd == 'quit':
                 welcome()
             else:
-                subprocess.run(f'ffmpeg {cmd}')
+                subprocess.run(f'ffmpeg {cmd}', check=False)
                 supercustom()
         supercustom()
-    if choice == 'X':
-    if choice == 'X':
-        subprocess.run('explorer.exe https://github.com/Lunar-Creators/Lunar-Media-Director')
+    if choice == 'X' or choice == 'x':
+        subprocess.run('explorer.exe https://github.com/Lunar-Creators/Lunar-Media-Director', check=False)
         welcome()
-    if choice == 'V':
+    if choice == 'V' or choice == 'v':
         void()
-    if choice == 'E':
-        void()
+    if choice == 'E' or choice == 'e':
+        leavemodule()
     else:
-        print('\033[1m\033[91m{}\033[0m'.format('INCORRECT INPUT'))
-        time.sleep(1)
-        os.system('cls' if os.name == 'nt' else 'clear')
+        wronginput()
         welcome()
+# NEED TO CLEAR TERMINAL BEFORE RUNNING A COMMAND
+os.system('cls' if os.name == 'nt' else 'clear')
 welcome()
-
